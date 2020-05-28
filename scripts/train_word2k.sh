7d1ef84681b606bfba19f8b51c44243ebe2864ca
Gopipo <gopipo@noreply.com>
2020-05-28 11:09:46 +0200

    =added training scripts and configs

---
+++ scripts/train_word2k.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+scripts=`dirname "$0"`
+base=$scripts/..
+
+models=$base/models
+configs=$base/configs
+
+mkdir -p $models
+
+num_threads=14
+device=5
+
+# measure time
+
+SECONDS=0
+
+CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/low_resource_word2k.yaml
+
+echo "time taken:"
+echo "$SECONDS seconds"
